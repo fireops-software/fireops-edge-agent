f6f8933e971df901e2f35548996fabd1758f6910
Lukas Neuhauser <neuhauser48@gmail.com>
2025-09-17 23:31:36 +0200

    Update log level

--- services/FireOpsOperator.go
+++ services/FireOpsOperator.go
@@ -118,27 +118,27 @@ func (f *FireOpsOperator) run() error {
 		// Route message based on message type
 		switch raw.MsgType {
 		case MESSAGE_TYPE_GET_CONTAINERS:
-			f.logger.Tracef("New incomming %s request %v", MESSAGE_TYPE_GET_CONTAINERS, string(raw.Body))
+			f.logger.Debugf("New incomming %s request %v", MESSAGE_TYPE_GET_CONTAINERS, string(raw.Body))
 			if err := routeMsg(ws, f.sendMessage, raw, f.handleGetDeploymentRequest); err != nil {
 				return err
 			}
 		case MESSAGE_TYPE_INSTALL:
-			f.logger.Tracef("New incomming %s request %v", MESSAGE_TYPE_INSTALL, string(raw.Body))
+			f.logger.Debugf("New incomming %s request %v", MESSAGE_TYPE_INSTALL, string(raw.Body))
 			if err := routeMsg(ws, f.sendMessage, raw, f.handleInstallRequest); err != nil {
 				return err
 			}
 		case MESSAGE_TYPE_DESTROY:
-			f.logger.Tracef("New incomming %s request %v", MESSAGE_TYPE_DESTROY, string(raw.Body))
+			f.logger.Debugf("New incomming %s request %v", MESSAGE_TYPE_DESTROY, string(raw.Body))
 			if err := routeMsg(ws, f.sendMessage, raw, f.handleDestroyRequest); err != nil {
 				return err
 			}
 		case MESSAGE_TYPE_GET_CONTAINER_LOGS:
-			f.logger.Tracef("New incomming %s request %v", MESSAGE_TYPE_GET_CONTAINER_LOGS, string(raw.Body))
+			f.logger.Debugf("New incomming %s request %v", MESSAGE_TYPE_GET_CONTAINER_LOGS, string(raw.Body))
 			if err := routeMsg(ws, f.sendMessage, raw, f.handleGetLogsRequest); err != nil {
 				return err
 			}
 		case MESSAGE_TYPE_GET_AGENT_VERSION:
-			f.logger.Tracef("New incomming %s request %v", MESSAGE_TYPE_GET_AGENT_VERSION, string(raw.Body))
+			f.logger.Debugf("New incomming %s request %v", MESSAGE_TYPE_GET_AGENT_VERSION, string(raw.Body))
 			if err := routeMsg(ws, f.sendMessage, raw, f.handleGetAgentVersionRequest); err != nil {
 				return err
 			}
@@ -172,35 +172,35 @@ func routeMsg[I, O any](ws *websocket.Conn, sendFunc func(*websocket.Conn, any) 
 }
 
 func (f *FireOpsOperator) cleanDocker(ctx context.Context) error {
-	f.logger.Tracef("Listing all containers...")
+	f.logger.Debugf("Listing all containers...")
 	containers := <-f.dockerApi.ListContainers(ctx)
 	if containers.Error != nil {
 		return containers.Error
 	}
-	f.logger.Tracef("List of all containers runnning in context: %v", containers.Result)
+	f.logger.Debugf("List of all containers runnning in context: %v", containers.Result)
 	// Remove all containers
 	for _, container := range containers.Result {
-		f.logger.Tracef("Removing container %v...", container.Names)
+		f.logger.Debugf("Removing container %v...", container.Names)
 		remove := <-f.dockerApi.RemoveContainer(ctx, container.ID)
 		if remove.Error != nil {
 			return remove.Error
 		}
-		f.logger.Tracef("Container %v removed", container.Names)
+		f.logger.Debugf("Container %v removed", container.Names)
 	}
 	// Prune images
-	f.logger.Tracef("Prune images...")
+	f.logger.Debugf("Prune images...")
 	imagePrune := <-f.dockerApi.PruneImages(ctx, true)
 	if imagePrune.Error != nil {
 		return imagePrune.Error
 	}
-	f.logger.Tracef("Images pruned %v", imagePrune.Result)
+	f.logger.Debugf("Images pruned %v", imagePrune.Result)
 	// Prune networks
-	f.logger.Tracef("Prune networks...")
+	f.logger.Debugf("Prune networks...")
 	netPrune := <-f.dockerApi.PruneNetworks(ctx)
 	if netPrune.Error != nil {
 		return netPrune.Error
 	}
-	f.logger.Tracef("Networks pruned %v", netPrune.Result)
+	f.logger.Debugf("Networks pruned %v", netPrune.Result)
 	// Return success
 	return nil
 }
@@ -213,9 +213,9 @@ func (f *FireOpsOperator) handleGetDeploymentRequest(msg wsRequest[getDeployment
 	ctx, cancel := context.WithTimeout(f.ctx, 10*time.Second)
 	defer cancel()
 	// Get Running containers from docker api
-	f.logger.Tracef("Listing all containers...")
+	f.logger.Debugf("Listing all containers...")
 	containers := <-f.dockerApi.ListContainers(ctx)
-	f.logger.Tracef("List of all containers runnning in context: %v", containers.Result)
+	f.logger.Debugf("List of all containers runnning in context: %v", containers.Result)
 	// Return current state
 	return wsResponse[getDeploymentResponse]{
 		MsgId:   msg.MsgId,
@@ -238,7 +238,7 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 		}
 	}
 	// Create Network
-	f.logger.Tracef("Creating docker network %s...", f.fireopsNetwork)
+	f.logger.Debugf("Creating docker network %s...", f.fireopsNetwork)
 	dockerNet := <-f.dockerApi.CreateNetwork(ctx, f.fireopsNetwork, network.NetworkBridge)
 	if dockerNet.Error != nil {
 		return wsResponse[installResponse]{
@@ -247,7 +247,7 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 			Error:   dockerNet.Error,
 		}
 	}
-	f.logger.Tracef("Nework %s created with id %s", f.fireopsNetwork, dockerNet.Result.ID)
+	f.logger.Debugf("Nework %s created with id %s", f.fireopsNetwork, dockerNet.Result.ID)
 	// Install services
 	for _, service := range msg.Body {
 		env := []string{}
@@ -255,7 +255,7 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 			env = append(env, fmt.Sprintf("%s=%s", k, v))
 		}
 		// Create container
-		f.logger.Tracef("Creating docker container %s...", service.ServiceName)
+		f.logger.Debugf("Creating docker container %s...", service.ServiceName)
 		create := <-f.dockerApi.CreateContainer(
 			ctx,
 			service.Image,
@@ -275,9 +275,9 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 				Error:   create.Error,
 			}
 		}
-		f.logger.Tracef("Container %s created with id %s", service.ServiceName, create.Result.ID)
+		f.logger.Debugf("Container %s created with id %s", service.ServiceName, create.Result.ID)
 		// Start container
-		f.logger.Tracef("Starting docker container: %s...", service.ServiceName)
+		f.logger.Debugf("Starting docker container: %s...", service.ServiceName)
 		start := <-f.dockerApi.StartContainer(ctx, create.Result.ID)
 		if start.Error != nil {
 			return wsResponse[installResponse]{
@@ -286,10 +286,10 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 				Error:   start.Error,
 			}
 		}
-		f.logger.Tracef("Container %s started", service.ServiceName)
+		f.logger.Debugf("Container %s started", service.ServiceName)
 	}
 	// Get containers
-	f.logger.Tracef("List running containers...")
+	f.logger.Debugf("List running containers...")
 	containers := <-f.dockerApi.ListContainers(ctx)
 	if containers.Error != nil {
 		return wsResponse[installResponse]{
@@ -298,7 +298,7 @@ func (f *FireOpsOperator) handleInstallRequest(msg wsRequest[installRequest]) ws
 			Error:   containers.Error,
 		}
 	}
-	f.logger.Tracef("Running containers: %v", containers.Result)
+	f.logger.Debugf("Running containers: %v", containers.Result)
 	// Return result
 	return wsResponse[installResponse]{
 		MsgId:   msg.MsgId,
